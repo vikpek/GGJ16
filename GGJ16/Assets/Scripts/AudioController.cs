1d625e95b54e10ada1d318baa9363ce4928848e7
Viktor Pekar <vikpek@gmail.com>
2016-01-30 12:09:25 +0100

    Added first Audiomixer functionality

---
+++ GGJ16/Assets/Scripts/AudioController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AudioController : MonoBehaviour {
+
+	[SerializeField]
+	private AudioMixerGroup audioMixerGroup01;
+
+	[SerializeField]
+	private AudioMixerGroup audioMixerGroup02;
+
+
+
+}
