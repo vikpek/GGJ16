d733d517c0cf8e6719adf99e0e6676dd3e09be81
Viktor Pekar <vikpek@gmail.com>
2016-01-30 13:04:09 +0100

    Added proof of concept for audio mixing (with gothic soundtrack yay)

--- GGJ16/Assets/Scripts/AudioController.cs
+++ GGJ16/Assets/Scripts/AudioController.cs
@@ -10,6 +10,23 @@ public class AudioController : MonoBehaviour {
 	[SerializeField]
 	private AudioMixerGroup audioMixerGroup02;
 
+	bool dir;
 
+	void Start(){
+		dir = false;
+		InvokeRepeating ("SwitchTrack", 0f, 5f);
 
+	}
+
+	void SwitchTrack(){
+		if (dir) {
+			print (audioMixerGroup01.audioMixer.SetFloat ("volume01", 1f));
+			print (audioMixerGroup02.audioMixer.SetFloat ("volume02", -80f));
+			dir = false;
+		} else {
+			print (audioMixerGroup01.audioMixer.SetFloat ("volume01", -80f));
+			print (audioMixerGroup02.audioMixer.SetFloat ("volume02", 1f));
+			dir = true;
+		}
+	}
 }
