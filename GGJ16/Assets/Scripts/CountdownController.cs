062c605dfd65663c141dadf3799a29d97b9a3c03
Viktor Pekar <vikpek@gmail.com>
2016-01-31 08:56:06 +0100

    Added countdown sounds

--- GGJ16/Assets/Scripts/CountdownController.cs
+++ GGJ16/Assets/Scripts/CountdownController.cs
@@ -8,16 +8,24 @@ public class CountdownController : MonoBehaviour
     [SerializeField]
     private Text countDownText;
 
+	[SerializeField]
+	private AudioClip audioclipCountdown;
+
+	[SerializeField]
+	private AudioClip audioclipGo;
+
+	private AudioSource audioSource;
+
 	private RythmController rythmController;
 
 	void Start(){
-		rythmController = GameObject.FindGameObjectWithTag ("GameController").GetComponent<RythmController> ();
+		rythmController = GetComponent<RythmController> ();
+		audioSource = GetComponent<AudioSource> ();
 		StartCoroutine(GetReady());
 	}
 
 	private IEnumerator GetReady()    
 	{
-
         StartCoroutine(CountDown(3));
 
         yield return new WaitForSeconds(3.25f);
@@ -30,6 +38,7 @@ public class CountdownController : MonoBehaviour
         countDownText.text = countdown.ToString();
         countDownText.transform.localScale = Vector3.one * 3f;
         countDownText.transform.DOScale(Vector3.one, 0.33f);
+		audioSource.PlayOneShot (audioclipCountdown);
         yield return new WaitForSeconds(0.66f);
 
         if (countdown > 1)
@@ -40,9 +49,12 @@ public class CountdownController : MonoBehaviour
         else
         {
             countDownText.text = "GO!";
+
             countDownText.transform.localScale = Vector3.one * 3f;
             countDownText.transform.DOScale(Vector3.one, 0.33f);
+			audioSource.PlayOneShot (audioclipGo);
             yield return new WaitForSeconds(0.66f);
+
             countDownText.transform.localScale = Vector3.zero;
         }
     }
